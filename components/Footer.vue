5278e208352a17f93846b77eb3e2270f84aaa1fc
Riza Khan <khanriza@gmail.com>
2020-01-18 12:28:52 -0500

    Added some Footer styling, with responsive elements

---
+++ components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer">
+    <div class="section">1</div>
+    <div class="section">2</div>
+    <div class="section">3</div>
+    <div class="section">4</div>
+  </div>
+</template>
+
+<style scoped>
+  .footer {
+    width: 90%;
+    margin: auto;
+    border-top: 1px white solid;
+    padding: 10px 0;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  }
+
+</style>
