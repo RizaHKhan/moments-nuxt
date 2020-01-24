583d3a56b7acfc1e5fd91ec96a7e51667f5865fc
Riza Khan <khanriza@gmail.com>
2020-01-23 23:19:34 -0500

    Removed dummy data in store

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -7,9 +7,11 @@ exports.register = async (req, res) => {
   const user = new User(req.body)
 
   try {
-    await user.save()
+    await user.save().catch((err) => {
+      res.status(404).send({ err })
+    })
     res.send({ user })
   } catch (err) {
-    res.sendStatus(404).send({err})
+    res.status(404).send({ err })
   }
 }
--- store/events.js
+++ store/events.js
@@ -1,13 +1,7 @@
 import axios from 'axios'
 
 export const state = () => ({
-  events: [
-    { id: '1', name: 'Wordpress', price: 100 },
-    { id: '2', name: 'VueJS', price: 200 },
-    { id: '3', name: 'React', price: 300 },
-    { id: '4', name: 'Angular', price: 500 },
-    { id: '5', name: 'NodeJS', price: 5000 }
-  ],
+  events: [],
   errors: []
 })
 
